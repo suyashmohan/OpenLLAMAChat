13ec3b7305b662d789cd13271a18f87abdaad640
Suyash Mohan <suyashatweb@gmail.com>
2024-01-03 21:28:22 +0800

    try chatgpt

--- api/main.py
+++ api/main.py
@@ -1,40 +1,51 @@
+import os
 import asyncio
-from typing import AsyncIterable
+from typing import AsyncIterable, Awaitable
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from langchain.callbacks import AsyncIteratorCallbackHandler
-from langchain.chat_models.ollama import ChatOllama
+from langchain.chat_models.openai import ChatOpenAI
 from langchain.schema import HumanMessage
 
 app = FastAPI()
+os.environ["OPENAI_API_KEY"] = ""
 
 class Message(BaseModel):
     content: str
 
-async def send_message(content: str) -> AsyncIterable[str]:
+async def send_message(message: str) -> AsyncIterable[str]:
     callback = AsyncIteratorCallbackHandler()
-    model = ChatOllama(
-        model="mistral",
-        callbacks=[callback]
+    model = ChatOpenAI(
+        streaming=True,
+        verbose=True,
+        callbacks=[callback],
     )
 
-    task = asyncio.create_task(
-        model.agenerate(messages=[[HumanMessage(content=content)]])
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with a event to signal when it's done or an exception is raised."""
+        try:
+            await fn
+        except Exception as e:
+            # TODO: handle exception
+            print(f"Caught exception: {e}")
+        finally:
+            # Signal the aiter to stop.
+            event.set()
+
+    # Begin a task that runs in the background.
+    task = asyncio.create_task(wrap_done(
+        model.agenerate(messages=[[HumanMessage(content=message)]]),
+        callback.done),
     )
 
-    try:
-        async for token in callback.aiter():
-            yield token
-    except Exception as e:
-        print(f"Caught Exception: {e}")
-    finally:
-        callback.done.set()
-    
+    async for token in callback.aiter():
+        # Use server-sent-events to stream the response
+        yield f"{token}"
+
     await task
 
 @app.post("/api/chat")
 async def chat(message: Message):
-    generator = send_message(message.content)
-    return StreamingResponse(generator, media_type="text/event-stream")
+    return StreamingResponse(send_message(message.content), media_type="text/event-stream")
 
