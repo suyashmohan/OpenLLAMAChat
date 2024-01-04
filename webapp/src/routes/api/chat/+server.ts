15bd975988892556e40a4613d54ac007af58898b
Suyash Mohan <suyashatweb@gmail.com>
2024-01-04 22:42:14 +0800

    use LangChainStream

--- webapp/src/routes/api/chat/+server.ts
+++ webapp/src/routes/api/chat/+server.ts
@@ -1,42 +1,25 @@
-import { type Message, StreamingTextResponse } from "ai";
+import { LangChainStream, type Message, StreamingTextResponse } from "ai";
 import type { RequestHandler } from "./$types";
 import { ChatOllama } from "langchain/chat_models/ollama";
-import { PromptTemplate } from "langchain/prompts";
-import { BytesOutputParser } from "langchain/schema/output_parser";
-import { RunnableSequence } from "langchain/schema/runnable";
-
-const formatMessage = (m: Message) => {
-  return `${m.role}: ${m.content}`;
-};
-
-const TEMPLATE =
-  `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
-
-Current conversation:
-{chat_history}
-
-User: {input}
-AI:`;
+import { AIMessage, HumanMessage } from "langchain/schema";
 
 export const POST = (async ({ request }) => {
   const { messages }: { messages: Message[] } = await request.json();
-  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
-  const currentMessage = messages[messages.length - 1].content;
-  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+  const { stream, handlers } = LangChainStream();
 
   const model = new ChatOllama({
     model: "mistral",
     numCtx: 2048,
     stop: ["<|endoftext|>", "User:", "Assistant:", "System:"],
   });
-  const outputParser = new BytesOutputParser();
-  const chain = RunnableSequence.from([prompt, model, outputParser]);
-  const stream = await chain.stream({
-    chat_history: formattedPreviousMessages.join("\n"),
-    input: currentMessage,
-  });
 
-  return new StreamingTextResponse(
-    stream,
-  );
+  model.call(
+    messages.map((m) =>
+      m.role == "user" ? new HumanMessage(m.content) : new AIMessage(m.content)
+    ),
+    {},
+    [handlers],
+  ).catch(console.error);
+
+  return new StreamingTextResponse(stream);
 }) satisfies RequestHandler;
