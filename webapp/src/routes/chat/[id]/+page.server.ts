fd0fb2212fb34e553996e3f996b276d6a7fd5c80
Suyash Mohan <suyashatweb@gmail.com>
2024-03-23 10:06:40 +0800

    check for valid conversation id

--- webapp/src/routes/chat/[id]/+page.server.ts
+++ webapp/src/routes/chat/[id]/+page.server.ts
@@ -1,19 +1,30 @@
 import prisma from "$lib/prisma";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load = (async ({ params }) => {
   const conversationId = params.id;
-  let messages;
+
   if (conversationId) {
-    messages = await prisma.message.findMany({
-      orderBy: {
-        createdAt: "asc",
-      },
+    const conversation = await prisma.conversation.findFirst({
       where: {
-        conversationId,
+        id: conversationId,
       },
     });
+
+    if (conversation === null) {
+      redirect(302, "/chat/new");
+    }
   }
+
+  const messages = await prisma.message.findMany({
+    orderBy: {
+      createdAt: "asc",
+    },
+    where: {
+      conversationId,
+    },
+  });
 
   const conversations = await prisma.conversation.findMany({
     orderBy: {
